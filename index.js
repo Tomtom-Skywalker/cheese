c598d6f50227cdfcde63da3697a09f7a0aed9a0b
tomtomvader298 <40464123+Tomtom-Skywalker@users.noreply.github.com>
2022-12-08 12:21:03 +0000

    Updated index.js To Be V14 Ready

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //Do Not Change Anything Here, Unless You Know What You Are Doing This Is How We Connect To Discord And Read Commands!!
-const Discord = require("discord.js");
-//
-require('events').EventEmitter.defaultMaxListeners = 0
-//
-const client = new Discord.Client( { intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] } );
-const config = require("./config.json");
-const emojilist = require('./assets/emojilist.js');
+const fs = require('fs');
+const { Client, GatewayIntentBits, Partials, Collection, EmbedBuilder } = require('discord.js');
+const { token } = require('./config.json');
+const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 //
 client.on("ready", () =>{
     console.log(`Logged Into The Discord Client In ${client.ws.ping}ms!`);
@@ -15,50 +15,41 @@ client.on("ready", () =>{
         `CHEESE 🧀🧀🧀`,
         `Eating Cheese 🧀`,
         `🧀🧀🧀`,
-        `Prefix: [${config.prefix}]`,
-        `${emojilist.c}${emojilist.h}${emojilist.e}${emojilist.e}${emojilist.s}${emojilist.e} 🧀`,
-        `Help: [${config.prefix}help]`
+        `Help: [/help]`
             ];
       const Activity = [
-        `WATCHING`,
-        `PLAYING`,
-        `COMPETING`,
-        `LISTENING`,
+        0,
+        1,
+        2,
+        3,
+		5,
       ];
-      const status = statuses[Math.floor(Math.random() * statuses.length)];
+      const s = statuses[Math.floor(Math.random() * statuses.length)];
       const a = Activity[Math.floor(Math.random() * Activity.length)];
-      client.user.setActivity(status, { type: `${a}` });
+	  client.user.setPresence({ activities:  [{ name: `${s}` }], status: 'online' });
     }, 20000);
  })
+//const channel = client.channels.cache.get("ID"); this can be used to call a discord channel kept in index as a ref
+client.commands = new Collection();
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);}
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) { client.once(event.name, (...args) => event.execute(...args));
+    } else {client.on(event.name, (...args) => event.execute(...args));}
+}
 //
-client.config = config;
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-
-["commands", "events"].forEach(handler => {
-  require(`./handlers/${handler}`)(client);
+client.on('interactionCreate', async interaction => {
+	console.log(`${interaction.user.tag} in #${interaction.channel.name} from ${interaction.guild.name} triggered an interaction (Slash Command: ${interaction.commandName}) .`);
+	if (!interaction.isCommand()) return;
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return; try { await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: '```diff\n- OH NO! \n-A Error Has Occurred, If This Continues Please Contact Support (https://discord.gg/763gUAQDHq)! \n+Have A Nice Day 🙂 \nTomtomvader298 Was Here! \n```', ephemeral: true });
+	}
 });
-
-//===================================================\\
-client.on("guildCreate", guild => {
-  let server = guild.name
-  let found = 0;
-  guild.channels.cache.map((channel) => {
-      if (found === 0) {
-        if (channel.type === "GUILD_TEXT") {
-          if (channel.permissionsFor(client.user).has("VIEW_CHANNEL")) {
-            if (channel.permissionsFor(client.user).has("SEND_MESSAGES")) { 
-              let hi = new Discord.MessageEmbed().setColor(config.c)
-              .setTitle(`Hello ${server}, I Am ${client.user.username} [🧀]`).setURL("http://tomtomvader298.uk").setDescription("```diff\n+ Thanks For Adding Me You Have Made Me Blush! \n```").addField(`Im Happy To Of Been Invited To Join & Help ${server}`, `My Prefix Is "[${config.prefix}]", To Get Started Say: [${config.prefix} help]`)
-              channel.send({embeds:[hi]})
-              found = 1;
-            }
-          }
-        }
-      }
-    });
-})
-
-//======================================================\\
-
+//
+//------LOGIN------\\
 client.login(config.token);
